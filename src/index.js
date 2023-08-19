7114023f22485df50d81a387eb42d4dc543b859f
bexster <140751518+bexster@users.noreply.github.com>
2023-08-19 13:47:29 -0400

    Added hover style to card
    html: changed search button type; added class to search button;
    css: added hover transition to forecast

--- src/index.js
+++ src/index.js
@@ -145,7 +145,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "f09d3949047ab6c9e3bcaf79cf61f619";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
